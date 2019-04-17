a43059cf09bf12dbea0460560ee78aa04bbc5353
anhooo <cchpersoal@163.com>
2019-04-17 19:23:14 +0800

    spring-MVC

---
+++ src/main/java/org/smart4j/chapter2/model/Customer.java
@@ -0,0 +1,12 @@
+package org.smart4j.chapter2.model;
+
+public class Customer {
+    private long id;
+    private String contact;
+    private String name;
+    private String telephone;
+    private String email;
+    private String remark;
+
+    //省略 getter/setter 方法
+}
